9dceb25b22484ea1dc41c71a4a14f3d98469aaf4
wertrain <wertrain@gmail.com>
2017-04-24 00:09:04 +0900

    ベースコードを追加

---
+++ CharCodeConverter/Sources/GameMain.cpp
@@ -0,0 +1,35 @@
+#include <Windows.h>
+#include "..\Headers\GameMain.h"
+#include "..\Headers\Constants.h"
+
+/**
+* create game object
+*/
+bool Create(const HWND /*hWnd*/)
+{
+	return true;
+}
+
+/**
+* update frame
+*/
+void Update(const DWORD /*nowTime*/)
+{
+
+}
+
+/**
+* draw game object
+*/
+void Draw(const HDC /*hDC*/)
+{
+
+}
+
+/**
+* destroy game object
+*/
+void Destroy()
+{
+
+}
