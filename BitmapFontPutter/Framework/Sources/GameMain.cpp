2e7b3c3a94c047cbebff8be5768acbc7c32aeae8
wertrain <wertrain@gmail.com>
2017-06-07 00:45:08 +0900

    サンプルテキストを表示（太宰治 火の鳥より）

--- BitmapFontPutter/Framework/Sources/GameMain.cpp
+++ BitmapFontPutter/Framework/Sources/GameMain.cpp
@@ -4,6 +4,7 @@
 #include "..\Headers\GameMain.h"
 #include "..\Headers\Constants.h"
 
+#include "..\..\CharCodeConverter.h"
 #include "..\..\BitmapFont.h"
 
 BitmapFont* g_bitmapFont = nullptr;
@@ -14,7 +15,9 @@ BitmapFont* g_bitmapFont = nullptr;
 bool Create(const HWND /*hWnd*/)
 {
     g_bitmapFont = new BitmapFont();
-    return g_bitmapFont->Create(".\\Resources\\sjis.bmp");
+    if (!g_bitmapFont->Create(".\\Resources\\sjis.bmp"))
+        return false;
+    return true;
 }
 
 /**
@@ -30,7 +33,21 @@ void Update(const DWORD /*nowTime*/)
  */
 void Draw(const HDC hDC)
 {
-    g_bitmapFont->Draw(hDC);
+    int32_t x = 16, y = 32;
+    g_bitmapFont->DrawString(hDC, x, y, L"昔の話である。須々木乙彦は古着屋へはいって、"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"君のところに黒の無地の羽織はないか、と言った。"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「セルなら、ございます。」"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"昭和五年の十月二十日、東京の街路樹の葉は、風に散りかけていた。"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「まだセルでも、おかしくないか。」"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「もっともっとお寒くなりましてからでも、"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"黒の無地なら、おかしいことはございませぬ。」"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「よし。見せて呉れ。」"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「あなたさまがお召しになるので？」"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"角帽をあみだにかぶり、袖口がぼろぼろの学生服を着ていた。"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「そうだ。」差し出されたセルの羽織をその学生服の上にさっと羽織って、"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「短かくないか。」五尺七寸ほどの、痩せてひょろ長い大学生であった。"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「セルのお羽織なら、かえって少し短かめのほうが。」"); y += BitmapFont::FONT_CHAR_HEIGHT;
+    g_bitmapFont->DrawString(hDC, x, y, L"「粋か。いくらだ。」"); y += BitmapFont::FONT_CHAR_HEIGHT;
 }
 
 /**
@@ -38,7 +55,10 @@ void Draw(const HDC hDC)
  */
 void Destroy()
 {
-    g_bitmapFont->Destroy();
-    delete g_bitmapFont;
-    g_bitmapFont = nullptr;
+    if (g_bitmapFont != nullptr)
+    {
+        g_bitmapFont->Destroy();
+        delete g_bitmapFont;
+        g_bitmapFont = nullptr;
+    }
 }
